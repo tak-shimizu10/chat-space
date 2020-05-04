270a47697dd1446a9ac387a7bff2763010db8684
tak-shimizu10 <tak.shim10@gmail.com>
2020-05-04 15:58:57 +0900

    Preparation deploy_4

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -51,11 +51,11 @@
 # ------------------------------------
 server "54.150.216.103",
   user: "ec2-user",
-  roles: %w{app db web},
-  ssh_options: {
-    user: "user_name", # overrides user setting above
-    keys: %w(/home/user_name/.ssh/id_rsa),
-    forward_agent: false,
-    auth_methods: %w(publickey password)
-    # password: "please use keys"
-  }
+  roles: %w{app db web}
+  # ssh_options: {
+  #   user: "user_name", # overrides user setting above
+  #   keys: %w(/home/user_name/.ssh/id_rsa),
+  #   forward_agent: false,
+  #   auth_methods: %w(publickey password)
+  #   # password: "please use keys"
+  # }
